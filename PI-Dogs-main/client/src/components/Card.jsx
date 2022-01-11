c22121de1b36c0c3266dbcf179013f02556918d8
andrea198824 <andreguti88@gmail.com>
2022-01-11 13:52:43 -0500

    front2
    -

--- PI-Dogs-main/client/src/components/Card.jsx
+++ PI-Dogs-main/client/src/components/Card.jsx
@@ -11,21 +11,3 @@ export default function Card({name, image, height, weight }){
         </div>
     );
 }
-
-reducer
-
-const initialState ={
-    dogs :[]
-}
-export function rootReducer(state = initialState, action){
-    switch(action.type) {
-        case 'GET_DOGS':
-            return {
-                ...state,
-                dogs: action.payload
-    }
-    default:
-        return state;
-    }
-}
-export default rootReducer;
