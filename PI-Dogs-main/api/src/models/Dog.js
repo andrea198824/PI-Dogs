a10e1739243810f413e697cd567b98ed502759dc
andrea198824 <andreguti88@gmail.com>
2022-01-11 13:04:45 -0500

    Inicio
    -

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.STRING,
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    image:{
+      type: DataTypes.STRING,
+    }
   });
 };
